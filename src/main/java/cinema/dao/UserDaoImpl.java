eb1632ccaa4f46806b0523a3f5caafa90d5c4d13
Tonitrus <grom4er@gmail.com>
2021-02-02 17:19:10 +0200

    Add UserDaoImpl implemet's methods

--- src/main/java/cinema/dao/UserDaoImpl.java
+++ src/main/java/cinema/dao/UserDaoImpl.java
@@ -1,16 +1,45 @@
 package cinema.dao;
 
+import cinema.exception.DataProcessingException;
+import cinema.lib.Dao;
 import cinema.model.User;
+import cinema.util.HibernateUtil;
 import java.util.Optional;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+@Dao
 public class UserDaoImpl implements UserDao {
     @Override
     public User add(User user) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add user: " + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public Optional<User> findByEmail(String email) {
-        return Optional.empty();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from User where email=:email", User.class)
+                    .setParameter("email", email)
+                    .uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find user by email " + email + ". ", e);
+        }
     }
 }
