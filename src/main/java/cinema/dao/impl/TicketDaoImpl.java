9d169c6186f1b6a91a801316db0cbb372780e23b
Tonitrus <grom4er@gmail.com>
2021-02-03 14:29:16 +0200

    Create TicketDao

---
+++ src/main/java/cinema/dao/impl/TicketDaoImpl.java
@@ -0,0 +1,11 @@
+package cinema.dao.impl;
+
+import cinema.dao.TicketDao;
+import cinema.model.Ticket;
+
+public class TicketDaoImpl implements TicketDao {
+    @Override
+    public Ticket add(Ticket ticket) {
+        return null;
+    }
+}
