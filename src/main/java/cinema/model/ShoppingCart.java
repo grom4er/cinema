c5a07f8b58ee2d7cfc89037e2b8d5cbd3a5b00f6
Tonitrus <grom4er@gmail.com>
2021-02-03 14:25:57 +0200

    Create and fill Ticket

--- src/main/java/cinema/model/ShoppingCart.java
+++ src/main/java/cinema/model/ShoppingCart.java
@@ -14,6 +14,41 @@ public class ShoppingCart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private List<Ticket> tickets;
+
     private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" +
+                "id=" + id +
+                ", tickets=" + tickets +
+                ", user=" + user +
+                '}';
+    }
 }
