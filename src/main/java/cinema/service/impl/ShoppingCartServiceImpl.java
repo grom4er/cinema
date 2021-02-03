191c9123eec790f351ae15967b7cc6b5f8b76f01
Tonitrus <grom4er@gmail.com>
2021-02-03 14:33:54 +0200

    Create ShoppingCartService Change derictory of service impl

---
+++ src/main/java/cinema/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,28 @@
+package cinema.service.impl;
+
+import cinema.model.MovieSession;
+import cinema.model.ShoppingCart;
+import cinema.model.User;
+import cinema.service.ShoppingCartService;
+
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return null;
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+
+    }
+}
