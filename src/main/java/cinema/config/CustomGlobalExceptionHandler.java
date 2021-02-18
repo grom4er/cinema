ff4c065b05035318a88a3c969c6cd04ef8e2757f
Tonitrus <grom4er@gmail.com>
2021-02-18 16:20:14 +0200

    Fix Handler annotation

--- src/main/java/cinema/config/CustomGlobalExceptionHandler.java
+++ src/main/java/cinema/config/CustomGlobalExceptionHandler.java
@@ -10,9 +10,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
